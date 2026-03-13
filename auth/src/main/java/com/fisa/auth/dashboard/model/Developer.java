f491e6ff39507dc975a36b1dce9e6f2c8c802068
HeeYeon-Ko <rhgmldus6@gmail.com>
2026-03-13 11:31:18 +0900

    entity & repository 생성

---
+++ auth/src/main/java/com/fisa/auth/dashboard/model/Developer.java
@@ -0,0 +1,52 @@
+package com.fisa.auth.dashboard.model;
+
+import com.fisa.auth.authentication.model.Users;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+
+import lombok.*;
+
+@Entity
+@Table(name = "developers")
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Developer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false, unique = true)
+    private Users user;
+
+    @Column(name = "company_name", length = 100)
+    private String companyName;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+
+    @PrePersist
+    public void prePersist() {
+        if (this.createdAt == null) {
+            this.createdAt = LocalDateTime.now();
+        }
+    }
+
+    public void changeCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+}
